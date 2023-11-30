39d34a625f271ee12a67fb3bcaa749685157f7de
MichaelVacca <michaelvacca07@gmail.com>
2023-11-30 00:20:47 -0500

    Rebased and added signup page

--- ateliermecanique-ws/src/main/resources/schema-mysql.sql
+++ ateliermecanique-ws/src/main/resources/schema-mysql.sql
@@ -11,6 +11,28 @@ create table if not exists customers(
     customer_password CHAR(60) -- for BCrypt hashing
 );
 
+CREATE TABLE IF NOT EXISTS users (
+                                     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                                     username VARCHAR(50) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    enabled BOOLEAN NOT NULL DEFAULT TRUE
+    );
+
+-- Roles table
+CREATE TABLE IF NOT EXISTS roles (
+                                     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                                     name VARCHAR(50) NOT NULL
+    );
+
+-- Users_roles join table
+CREATE TABLE IF NOT EXISTS users_roles (
+                                           user_id INT NOT NULL,
+                                           role_id INT NOT NULL,
+                                           PRIMARY KEY (user_id, role_id),
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (role_id) REFERENCES roles (id)
+    );
+
 create table if not exists vehicles(
                                         id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
                                         vehicle_id VARCHAR(36),
