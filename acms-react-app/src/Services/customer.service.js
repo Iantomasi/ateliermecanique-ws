c92bace79e119b99d8c3bebfb102ceff6f8dd04a
Michael  Vacca <83475558+MichaelVacca@users.noreply.github.com>
2024-01-23 18:34:23 -0500

    Deployment (#42)
    * Changed url for java backend deployment
    
    * Added back end url to cors
    
    * changed back end url to cors
    
    * Added more .env files for deployed and local back end

--- acms-react-app/src/Services/customer.service.js
+++ acms-react-app/src/Services/customer.service.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 
-const API_CUSTOMERS_URL = "http://localhost:8080/api/v1/customers";
+const API_BASE_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:8080";
+const API_CUSTOMERS_URL = `${API_BASE_URL}/api/v1/customers`;
 
-class customerService{
+class customerService {
 
     updateCustomer(id, customer){
-        return axios.put(API_CUSTOMERS_URL + `/${id}`, customer, { headers: authHeader() });
+        return axios.put(`${API_CUSTOMERS_URL}/${id}`, customer, { headers: authHeader() });
     }
 
 }
--- acms-react-app/src/Services/user.service.js
+++ acms-react-app/src/Services/user.service.js
@@ -1,24 +1,26 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 
-const API_URL = "http://localhost:8080/api/v1/content/";
-const API_AVAILABILITY_URL = "http://localhost:8080/api/v1/availability/";
-class userService{
+const API_BASE_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:8080";
+const API_CONTENT_URL = `${API_BASE_URL}/api/v1/content/`;
+const API_AVAILABILITY_URL = `${API_BASE_URL}/api/v1/availability/`;
+
+class userService {
 
     getPulicContent(){
-        return axios.get(API_URL + 'all');
+        return axios.get(`${API_CONTENT_URL}all`);
     }
 
     getUserBoard(){
-        return axios.get(API_URL + 'user',{headers:authHeader()});
+        return axios.get(`${API_CONTENT_URL}user`, { headers: authHeader() });
     }
 
     getAdminContent(){
-        return axios.get(API_URL + 'admin',{headers:authHeader()});
+        return axios.get(`${API_CONTENT_URL}admin`, { headers: authHeader() });
     }
 
     getAvailabilities(date){
-        return axios.get(API_AVAILABILITY_URL + `${date}`,{headers:authHeader()});
+        return axios.get(`${API_AVAILABILITY_URL}${date}`, { headers: authHeader() });
     }
 }
 
