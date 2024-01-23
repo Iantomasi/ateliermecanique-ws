05ec57cfcf953a68b177726539ff201025e6dbb6
Giuliano <giulianoiantomasi@gmail.com>
2024-01-23 11:21:11 -0500

    remote commit

--- ateliermecanique-ws/src/main/java/com/champlain/ateliermecaniquews/customerinvoicemanagementsubdomain/businesslayer/CustomerInvoiceService.java
+++ ateliermecanique-ws/src/main/java/com/champlain/ateliermecaniquews/customerinvoicemanagementsubdomain/businesslayer/CustomerInvoiceService.java
@@ -1,4 +1,12 @@
 package com.champlain.ateliermecaniquews.customerinvoicemanagementsubdomain.businesslayer;
 
+import com.champlain.ateliermecaniquews.customerinvoicemanagementsubdomain.presentationlayer.CustomerInvoiceResponseModel;
+
+import java.util.List;
+
 public interface CustomerInvoiceService {
+
+    List<CustomerInvoiceResponseModel> getAllInvoices();
+
+
 }
