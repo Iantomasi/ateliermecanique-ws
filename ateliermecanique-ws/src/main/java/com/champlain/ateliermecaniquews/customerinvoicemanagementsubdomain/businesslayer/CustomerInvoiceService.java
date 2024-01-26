bcca4244468d1aefcc04758323941d3fbf79a71f
Giuliano <giulianoiantomasi@gmail.com>
2024-01-26 01:48:46 -0500

    rebasing and pull request

--- ateliermecanique-ws/src/main/java/com/champlain/ateliermecaniquews/customerinvoicemanagementsubdomain/businesslayer/CustomerInvoiceService.java
+++ ateliermecanique-ws/src/main/java/com/champlain/ateliermecaniquews/customerinvoicemanagementsubdomain/businesslayer/CustomerInvoiceService.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface CustomerInvoiceService {
 
-    List<CustomerInvoiceResponseModel> getAllInvoices(); // test change
+    List<CustomerInvoiceResponseModel> getAllInvoices();
 
 
 
