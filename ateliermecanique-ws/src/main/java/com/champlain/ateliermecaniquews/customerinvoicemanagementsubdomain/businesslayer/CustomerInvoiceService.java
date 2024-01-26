a36eead0d52e651a11fa6a77f1ab5e269d5cf18d
Giuliano <giulianoiantomasi@gmail.com>
2024-01-26 01:48:16 -0500

    ticket complete 2

--- ateliermecanique-ws/src/main/java/com/champlain/ateliermecaniquews/customerinvoicemanagementsubdomain/businesslayer/CustomerInvoiceService.java
+++ ateliermecanique-ws/src/main/java/com/champlain/ateliermecaniquews/customerinvoicemanagementsubdomain/businesslayer/CustomerInvoiceService.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface CustomerInvoiceService {
 
-    List<CustomerInvoiceResponseModel> getAllInvoices();
+    List<CustomerInvoiceResponseModel> getAllInvoices(); // test change
 
 
 
