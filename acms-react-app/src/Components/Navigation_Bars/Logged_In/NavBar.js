51cad6200fcc41d03c687ece64ef75d9f04bab78
Cristian <cf93082@gmail.com>
2024-02-17 19:12:24 -0500

    fixed

--- acms-react-app/src/Components/Navigation_Bars/Logged_In/NavBar.js
+++ acms-react-app/src/Components/Navigation_Bars/Logged_In/NavBar.js
@@ -23,9 +23,9 @@ function Navbar() {
         </div>
 
           <div className="flex items-center">
-            <a href="/about" className="text-black px-4">About</a>
-            <a href="/services" className="text-black px-4">Services</a>
-            <a href="/contact" className="text-black px-4">Contact</a>
+          <a href="/about" className="text-gray-700 hover:text-gray-900 px-2 py-1">About</a>
+          <a href="/services" className="text-gray-700 hover:text-gray-900 px-2 py-1">Services</a>
+          <a href="/contact" className="text-gray-700 hover:text-gray-900 px-2 py-1">Contact</a>
           </div>
         </div>
 
