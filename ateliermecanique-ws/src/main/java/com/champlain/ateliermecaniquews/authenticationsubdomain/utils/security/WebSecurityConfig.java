84fcd90653322dede81cde2164ba89b10b70af61
Cristian <cf93082@gmail.com>
2024-02-21 22:34:09 -0500

    fixing cors

--- ateliermecanique-ws/src/main/java/com/champlain/ateliermecaniquews/authenticationsubdomain/utils/security/WebSecurityConfig.java
+++ ateliermecanique-ws/src/main/java/com/champlain/ateliermecaniquews/authenticationsubdomain/utils/security/WebSecurityConfig.java
@@ -81,7 +81,7 @@ public class WebSecurityConfig{
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://atelier--mecanique.online","http://216.128.185.153"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000","http://216.128.185.153", "https://atelier--mecanique.online"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
